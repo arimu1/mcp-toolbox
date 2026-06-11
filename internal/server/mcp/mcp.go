32f16b4ac673a249e88265e3bf9ef377adb69d23
duwenxin <duwenxin@google.com>
2026-06-11 13:26:50 -0400

    add integration test

--- internal/server/mcp/mcp.go
+++ internal/server/mcp/mcp.go
@@ -53,6 +53,7 @@ func ProcessMethod(ctx context.Context, mcpVersion string, id jsonrpc.RequestId,
 		return jsonrpc.NewError(id, jsonrpc.INTERNAL_ERROR, err.Error(), nil), err
 	}
 	switch mcpVersion {
+<<<<<<< HEAD
 	case mcputil.VERSION_DRAFT:
 		if enableDraft {
 			return vdraft.ProcessMethod(ctx, id, method, g, primitiveMgr, body, header)
